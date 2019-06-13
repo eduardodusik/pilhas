8b5db1a5f1f3215fdc328466c716cc5d52529087
Eduardo Gracez Dusik <42807647+eduardodusik@users.noreply.github.com>
2019-06-13 00:38:22 -0300

    ffoifoifoif

--- trabalho_g2_eduardo_dusik.cpp
+++ trabalho_g2_eduardo_dusik.cpp
@@ -57,10 +57,92 @@ void retira_3(struct nodo **pilha1, int cont){
 	*pilha1 = aux1;
 }
 
-int main () {
+
+void enfileirar (nodo **fila, int valor) {
+	struct nodo *auxi;
+	struct nodo *x = (nodo *)malloc(sizeof(struct nodo));
+	x->dados = valor;
+	x->proximo = NULL;
+	
+	if(*fila == NULL){
+		*fila = x;
+	}else {
+		auxi = *fila;
+		while (auxi->proximo != NULL){
+			auxi = auxi->proximo;
+		}
+		auxi->proximo = x;
+	}	
+}
+
+int desenfileirar (nodo **fila){
+	int valor;
+	struct nodo *aux;
+	
+	aux = *fila;
+	valor = aux->dados;
+	*fila = aux->proximo;
+	free(aux);
+	return valor;
+}
+
+void maior_10(struct nodo **fila1, struct nodo **fila2, struct nodo **fila3){
+	struct nodo *aux1;
+	struct nodo *aux2;
+	
+	aux1 = *fila1;
+	aux2 = *fila2;
+	
+	while(aux1 != NULL){
+		if(aux1->dados > 10){
+			enfileirar(fila3, aux1->dados);
+			aux1 = aux1->proximo;
+		}else {
+			aux1 = aux1->proximo;
+		}
+	}
+	
+	while(aux2 != NULL){
+		if(aux2->dados > 10){
+			enfileirar(fila3, aux2->dados);
+			aux2 = aux2->proximo;
+		}else {
+			aux2 = aux2->proximo;
+		}
+	}
+}
+
+void filas (){
+	struct nodo *fila1;
+	struct nodo *fila2;
+	struct nodo *fila3;
+	int valor = 1;
+	
+	while(valor !=0){
+		printf("Digite um valor para armazenar na fila 1 (0 para sair): ");
+		scanf("%i", &valor);
+		if(valor != 0){
+			enfileirar(&fila1, valor);
+		}
+	}
+	valor = 1;
+	while(valor != 0){
+		printf("Digite um valor para armazenar na fila 2 (0 para sair): ");
+			scanf("%i", &valor);
+		if(valor !=0){
+			enfileirar(&fila2, valor);
+		}
+	} 
+	mostrar_pilha(&fila3);
+	
+	
+}
+
+void pilhas () {
 	struct nodo *pilha1;
 	int valor=1;
 	int cont = 0;
+	
 	while(valor != 0){
 		printf("Digite um valor para armazenar na pilha 1 e zero (0): ");
 		scanf("%i", &valor);
@@ -71,5 +153,29 @@ int main () {
 	
 	}
 	retira_3(&pilha1, cont);
-	mostrar_pilha(&pilha1);
+	mostrar_pilha(&pilha1);	
+}
+
+
+
+int main () {
+	int resposta = 1;
+	while(resposta != 0){
+		printf("Digite uma opcao:\n1 - Pilhas\n2 - Filas\n\nSua opcao: ");
+		scanf("%i", &resposta);
+		switch (resposta){
+			case 1:
+				pilhas();
+				break;
+			case 2:
+				filas();
+				break;
+			case 0:
+				printf("Voce escolheu sair :(");
+				break;
+			default:
+				printf("Escolha uma opcao: ");
+		}
+	}
+	
 }
