1af6591304b3641b7751d40277463520c78fe89a
Eduardo Gracez Dusik <42807647+eduardodusik@users.noreply.github.com>
2019-06-12 23:51:10 -0300

    vai fiao

--- trabalho_g2_eduardo_dusik.cpp
+++ trabalho_g2_eduardo_dusik.cpp
@@ -31,8 +31,8 @@ int pop (struct nodo **topo){
 
 void mostrar_pilha(struct nodo **fila){
   struct nodo *aux;
-
   aux=*fila;
+  
   while (aux != NULL)
   {
     printf("Valores-> %i\n",aux->dados);
@@ -40,54 +40,36 @@ void mostrar_pilha(struct nodo **fila){
   }
 }
 
-void maior_10(struct nodo **pilha1, struct nodo **pilha2, struct nodo **pilha3){
-	struct nodo *aux1;
-	aux1 = *pilha1;
-	while(aux1){
-		push(pilha3, aux1->dados);
-		pop(&aux1);
-	}
 
-/*	while((*pilha1) != NULL){
-		if((*pilha1)->dados >= 10 && (*pilha1) != NULL){
-			push(pilha3, (*pilha1)->dados);
+void retira_3(struct nodo **pilha1, int cont){
+	struct nodo *aux1;
+	aux1 = NULL;
+	while(cont > 0){
+		if((*pilha1)->dados % 3 ==0){
 			pop(pilha1);
-		}else if(*pilha1 != NULL){
+			cont--;
+		}else if (cont != 0){
+			push(&aux1, (*pilha1)->dados);
 			pop(pilha1);
+			cont--;
 		}
 	}
-	printf("cheguei a sair do laço");
-	while(*pilha2 != NULL){
-		printf("bah cheguei aqui tlgd");
-		if((*pilha2)->dados >= 10 && *pilha2 != NULL){
-			printf("entrei aqui 2");
-			push(pilha3, (*pilha2)->dados);
-			pop(pilha2);
-		}else if(*pilha2 != NULL){
-			pop(pilha2);
-		}
-	} */
-	
+	*pilha1 = aux1;
 }
 
 int main () {
-	struct nodo *pilha1, *pilha2, *pilha3;
+	struct nodo *pilha1;
 	int valor=1;
+	int cont = 0;
 	while(valor != 0){
-		printf("Digite um valor para armazenar na pilha 1 e zero (0) para passar para pilha 2: ");
+		printf("Digite um valor para armazenar na pilha 1 e zero (0): ");
 		scanf("%i", &valor);
-		push(&pilha1, valor);
-		if(valor == 0){
-			valor  = 1;
-			while(valor != 0){
-				printf("Digite um valor para armazenar (pilha 2) e zero (0) para sair: ");
-				scanf("%i", &valor);
-				push(&pilha2, valor);
-			}
-		}
-		
+		if(valor !=0) {
+			cont++;
+			push(&pilha1, valor);
+		}		
 	
 	}
-	maior_10(&pilha1, &pilha2, &pilha3);
-	mostrar_pilha(&pilha3);
+	retira_3(&pilha1, cont);
+	mostrar_pilha(&pilha1);
 }
